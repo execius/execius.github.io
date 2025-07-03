eeb73b22ddd262e1de0c6e8f242dbae5a7ce17df
xec <imraneqbit@proton.me>
2025-07-03 22:06:13 +0000

    stylistic changes

---
+++ deploy-tools/deploy-feed.py
@@ -0,0 +1,48 @@
+
+import json
+from pathlib import Path
+
+FEED_DIR = Path("../feed/posts/")
+TEMPLATE_FILE="index.html"
+feed_templates_files_tupple_lst={("../template.html","../index.html"),("../feed/template.html","../feed/index.html")}
+
+
+def load_feed_items():
+    items = []
+    for json_file in FEED_DIR.glob("*.json"):
+        with open(json_file, encoding="utf-8") as f:
+            data = json.load(f)
+            items.append(data)
+    return items
+
+def generate_feed_html(item):
+    return f"""\
+          <a class="feed-item" href="#">\
+            <img class="feed-subitem image" src="{item['image']}">\
+            <h2 class="feed-subitem title" >{item['title']}</h2>\
+            <p class="feed-subitem description">{item['description']}</p>\
+            <div class="feed-subitem information">\
+              <div class="feed-subitem information author">\
+                <img class="feed-subitem round_image" src="{item['author-profile']}" height=30 width=30>\
+                <p class="button feed-subitem information author-username">{item['author-username']}</p>\
+              </div>\
+              <p class="button feed-subitem information posting-date">{item['date']}</p>\
+
+            </div>\
+          </a>\
+    """
+
+def build_page(feed_items,file_template_tuple):
+    with open(file_template_tuple[0], encoding="utf-8") as f:
+        html = f.read()
+
+    feed_html = "\n".join(generate_feed_html(item) for item in feed_items)
+    html = html.replace("<!-- Feed-slot -->", feed_html)
+
+    with open(file_template_tuple[1], "w", encoding="utf-8") as f:
+        f.write(html)
+    print("Feed built successfully into", file_template_tuple[1])
+
+for file_templ_tup in feed_templates_files_tupple_lst:
+    feed_items = load_feed_items()
+    build_page(feed_items,file_templ_tup)
