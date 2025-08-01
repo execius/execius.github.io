e674819ff54e3539ddfed2c576a9a7ad0993c5e6
xec <imraneqbit@proton.me>
2025-08-01 14:09:34 +0000

    added topics based on tags

--- deploy-tools/deploy-feed.py
+++ deploy-tools/deploy-feed.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 import json
 from pathlib import Path
 import sys
@@ -16,6 +16,7 @@ jsons_path=rsc_path + "/json/"
 htmls_path=rsc_path+"/html"
 templates_path= htmls_path+ "/templates/"
 html_posts_path = htmls_path+"/posts/"
+topics_path = htmls_path+"/topics/"
 base_slot = "<!-- base-slot -->"
 
 try :
@@ -39,6 +40,7 @@ templates = {
         "post" : templates_path+"/post/template.html",
         "feed" : templates_path+"/feed/template.html",
         "topics" : templates_path+"/topics/template.html",
+        "topic" : templates_path+"/topic/template.html",
         "resources" : templates_path+"/resources/template.html",
         }
 json_files = {
@@ -63,6 +65,7 @@ def load_json_items(posts_file_json):
 
 
 posts_items = load_json_items(json_files['post'])
+topics_items = load_json_items(json_files['topics'])
 
 
 def get_text(file_path):
@@ -91,17 +94,20 @@ def make_page_text(template_text,html_text,replaced_text):
         result = template_text.replace(replaced_text,html_text)
         result = result.replace(base_slot,base_path)
         return result
-    except:
+    except Exception as e:
+        print(e)
         print("error making page")
         return -1
         pass
 def make_page_file(template_path,html_text,slot_text,result_path):
+    
     template_text = get_text(template_path)
     page_text = make_page_text(template_text,html_text,slot_text)
     write_text(result_path,page_text)
 
 def generate_feed_item(item):
 
+    html_posts_path = htmls_path+"/posts/"
     try :
         if sys.argv[1] == "main":
             base_path="/"
@@ -128,6 +134,7 @@ def generate_feed_html(items_array):
     feed_html = "\n".join(generate_feed_item(item) for item in items_array)
     return feed_html
 
+    return feed_html
 def build_feed(feed_items):
     feed_html=generate_feed_html(feed_items)
     feed_template=templates['feed']
@@ -151,7 +158,55 @@ def make_posts(posts_items):
         make_page_file(post_template,raw_post_text,slot_text,post_page_file)
 
 
+def make_topics(topics_items):
+    slot_text="<!-- feed-slot -->"
+    topic_name_slot = "<!-- topic-name-slot -->" 
+    topic_template = templates['topic']
+    for topic_item in topics_items:
+        topic_posts = []
+        topic_name = topic_item['title']
+        for item in posts_items:
+            post_topics = item['topics'].split(',')
+            print(post_topics)
+            if topic_name in post_topics:
+                topic_posts.append(item)
+        topic_feed_html = generate_feed_html(topic_posts)
+        print(topic_feed_html)
+        topic_page_file = topics_path+"/"+topic_item['title']+".html"
+        make_page_file(topic_template,topic_feed_html,slot_text,topic_page_file)
+        make_page_file(topic_page_file,topic_name,topic_name_slot,topic_page_file)
+
+
+def generate_topics_feed_item(item):
+    html_posts_path = htmls_path+"/posts/"
+    try :
+        if sys.argv[1] == "main":
+            base_path="/"
+        html_posts_path="/rsc/html/posts/"
+    except:
+        pass
+    result = f"""\
+          <a class="feed-item" href="{topics_path}/{item['title']}.html">\
+            <img class="feed-subitem image" src="{item['image']}">\
+            <h2 class="feed-subitem title" >{item['title']}</h2>\
+            <p class="feed-subitem description">{item['description']}</p>\
+          </a>\
+    """
+    return result
+
+def generate_topics_feed_html(items_array):
+    feed_html = "\n".join(generate_topics_feed_item(item) for item in items_array)
+    return feed_html
+def build_topics_feed(topics_items):
+    topic_feed=generate_topics_feed_html(topics_items)
+    topics_template=templates['topics']
+    slot_text="<!-- topics-slot -->"
+    topics_page = html_pages_files['topics']
+    make_page_file(topics_template,topic_feed,slot_text,topics_page)
+    print("done writing to ",topics_page)
+
 make_posts(posts_items)
+make_topics(topics_items)
 build_feed(posts_items)
+build_topics_feed(topics_items)
 make_page_file(templates['resources'],"","",html_pages_files['resources'])
-make_page_file(templates['topics'],"","",html_pages_files['topics'])
